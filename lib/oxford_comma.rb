6e83125dcedf6151c5bfcd218af28e6ad23a2a38
Alan Krajina <alankrajina88@gmail.com>
2019-05-09 18:13:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,5 @@
 def oxford_comma(array)
-  new_array=[]
-  array.each do |value|
   
-   if value == array[2]
-     puts "nesto"
-    end
-  end
 end
 
 oxford_comma(["fiddleheads","okra","kohlrabi"])
