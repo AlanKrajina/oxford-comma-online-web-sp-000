c462caddc1bdfdea5e69e0e4432bf52039353bf7
Alan Krajina <alankrajina88@gmail.com>
2019-05-09 18:18:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
   if array.length == 1
     array.join
+    
     elsif array.length == 2
     array.join ("and")
     
-    else
+     else
 end
 
 oxford_comma(["fiddleheads","okra","kohlrabi"])
